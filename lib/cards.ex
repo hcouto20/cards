aa5ea77a803b83ad4d877e86446ae1abd227194e
hcouto20 <helder@brassun.com.br>
2021-02-12 16:15:18 -0300

    implementado tester usando assert

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,6 +2,10 @@ defmodule Cards do
   @moduledoc """
   Provides methods for creating and handling a deck of cards
   """
+
+  @doc """
+  Returns a list of strings representing a deck of playing cards - helder
+  """
   def create_deck do
     values = [
       "Ace",
@@ -22,10 +26,37 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+      "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+      "Three of Hearts", "Four of Hearts", "Five of Hearts",
+      "Ace of Diamonds", "Two of Diamonds", "Three of Diamonds",
+      "Four of Diamonds", "Five of Diamonds"]
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+  Divides a deck into a hand and the remainder of the deck
+  The `hand_size` argument indicates how many cards should
+  be in the hand.
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck,1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
